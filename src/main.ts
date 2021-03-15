38e5afdd1c0215542fa0d2247a7debfda2b89247
M. França <mcfranca@amazon.com>
2021-03-15 18:11:43 -0300

    Additional Files
    md files

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
